91c9e8240a39cd427f92ec9e539454c87c2739a1
Claudio Santos <claudiogsssantos@gmail.com>
2023-10-19 19:56:16 -0300

    feat: create menu

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,16 +1,48 @@
 import { Button } from "./button";
 import { Card, CardContent } from "./card";
-import { ShoppingCart, Menu } from 'react-feather';
+import { ShoppingCart, Menu, User, Gift, List, Home } from 'react-feather';
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 
 const Header = () => {
   return ( 
-    <Card className="flex justify-between bg-black p-[1.875rem] items-center">
-      <Button size="icon" variant="outline">
-        <Menu />
-      </Button>
+    <Card className="flex justify-between bg-slate-50 p-[1.875rem] items-center">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <Menu />
+          </Button>
+        </SheetTrigger>
 
-      <h1 className="font-semibold text-lg text-stone-200">CS Store</h1>
+        <SheetContent side="left">
+          <SheetHeader className="text-xl font-bold items-center">
+            Menu
+          </SheetHeader>
+          <div className="mt-4 flex flex-col gap-3">
+            <Button variant="outline" className=" w-full justify-start gap-2 font-bold">
+              <Home />
+                Home
+            </Button>
+
+            <Button variant="outline" className=" w-full justify-start gap-2 font-bold">
+              <User />
+                Login
+            </Button>
+
+            <Button variant="outline" className=" w-full justify-start gap-2 font-bold">
+              <Gift />
+                Offers
+            </Button>
+
+            <Button variant="outline" className=" w-full justify-start gap-2 font-bold">
+              <List />
+              Catalog
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="font-semibold text-lg text-gray-900">CS Store</h1>
 
       <Button size="icon" variant="outline">
         <ShoppingCart />
