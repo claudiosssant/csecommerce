b8ac7defd6c8548ac05d4b1aa587d9d0eb68e944
Claudio Santos <claudiogsssantos@gmail.com>
2023-10-31 23:09:32 -0300

    feat: ensure user goes to product page

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -3,6 +3,7 @@ import { Product } from "@prisma/client";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDown } from "react-feather";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -10,50 +11,52 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4 mt-8">
-      <div className="relative flex h-full w-full items-center rounded-lg bg-gray-200">
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-auto"
-          style={{
-            objectFit: "contain",
-          }}
-          alt={product.name}
-        />
-      {product.discountPercentage > 0 && (
-        <Badge className="absolute left-3 top-3 px-2 pt-[2px]">
-          <ArrowDown size={16}/> {product.discountPercentage}%
-        </Badge>
-      )}
-      </div>
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex flex-col gap-4 mt-8">
+        <div className="relative flex h-full w-full items-center rounded-lg bg-gray-200">
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-auto"
+            style={{
+              objectFit: "contain",
+            }}
+            alt={product.name}
+          />
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 pt-[2px]">
+            <ArrowDown size={16}/> {product.discountPercentage}%
+          </Badge>
+        )}
+        </div>
 
-      <div className="flex flex-col gap-1">
-        <p className=" w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold">
-          {product.name}
-        </p>
-        <div className="flex items-center justify-center gap-2">
-          {product.discountPercentage > 0 ? (
-            <>
+        <div className="flex flex-col gap-1">
+          <p className=" w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold">
+            {product.name}
+          </p>
+          <div className="flex items-center justify-center gap-2">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+                  R$ {product.totalPrice.toFixed(2)}
+                </p>
+
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                  R$ {Number(product.basePrice).toFixed(2)}
+                </p>
+              </>
+            ) : (
               <p className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-                R$ {product.totalPrice.toFixed(2)}
+                R$ {product.basePrice.toFixed(2)}
               </p>
-
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
-                R$ {Number(product.basePrice).toFixed(2)}
-              </p>
-            </>
-          ) : (
-            <p className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-              R$ {product.basePrice.toFixed(2)}
-            </p>
-          )}
-          ;
+            )}
+            ;
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
